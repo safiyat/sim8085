42f5f9c666b650ea670685c5f33782036b100e11
Safiyat Reza <reza.safiyat@gmail.com>
2013-10-11 20:58:15 +0530

    Changed the approach of input validation. Using QByteArrays now, instead. (*) still accepted.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,85 +2,98 @@
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) :
-	QMainWindow(parent),
-	ui(new Ui::MainWindow)
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
-	ui->setupUi(this);
-	QRegExp re("[_a-z*+()]+");
-	QRegExpValidator *v = new QRegExpValidator(re);
-	ui->input->setValidator(v);
+    ui->setupUi(this);
+    QRegExp re("[_a-z*+()]+");
+    QRegExpValidator *v = new QRegExpValidator(re);
+    ui->input->setValidator(v);
 
 }
 
 MainWindow::~MainWindow()
 {
-	delete ui;
+    delete ui;
 }
 
 void MainWindow::process()
 {
-	QString inputText = ui->input->text();
+//    QString inputText = ui->input->text();
 
-	QRegExp re("[_a-z]");
+    QRegExp re("[_a-z]");
 
-	if(inputText.count('(') != inputText.count(')'))
-	{
-		ui->radioButton->setText("Matching parentheses not found!");
-		ui->tableWidget->setColumnCount(ui->tableWidget->columnCount() - 1);
-		return;
-	}
-	if(inputText.contains("*+") || inputText.contains("+*") || inputText.contains("()"))
-	{
-		ui->radioButton->setText("Operators occuring together!");
-		ui->tableWidget->setColumnCount(ui->tableWidget->columnCount() - 1);
-		return;
-	}
-	if(!inputText.contains(re))
-	{
-		ui->radioButton->setText("NO input string!");
-		ui->tableWidget->setColumnCount(ui->tableWidget->columnCount() - 1);
-		return;
-	}
+    if(!ui->input->text().contains(re))
+    {
+        ui->radioButton->setText("NO input string!");
+        ui->tableWidget->setColumnCount(ui->tableWidget->columnCount() - 1);
+        return;
+    }
 
-	ui->radioButton->setText("All's Well!");
-	ui->radioButton->toggle();
+    QByteArray text = ui->input->text().toAscii();
+    int i, parenthesis, alcount;
+    i = parenthesis = alcount = 0;
 
-	QString inputSymbols;
-	int i = 0;
-	while(i < inputText.length())
-	{
-		if(inputText.toAscii().at(i) < 97)
-		{
-			i++;
-			continue;
-		}
-		if(i == 0)
-			inputSymbols.append(inputText.toAscii().at(i));
-		else
-		{
-			if (!inputSymbols.contains(inputText.toAscii().at(i)))
-				inputSymbols.append(inputText.toAscii().at(i));
-		}
-		i++;
-	}
+    if(text.contains("*+") || text.contains("+*") || text.contains("()"))
+        return;
 
-	QStringList list;
+    while( i < text.length())
+    {
+        if(text.at(i) == '(')
+            parenthesis++;
+        else if(text.at(i) == ')')
+            parenthesis--;
+        if(parenthesis < 0)
+            return;
 
-	ui->tableWidget->setColumnCount(inputSymbols.length());
+        if(isalpha(text.at(i)))
+            alcount++;
+        else if(((text.at(i) == '*') || (text.at(i) == '*')) && alcount == 0)
+            return;
+        i++;
+    }
+    if(alcount == 0 || parenthesis != 0)
+        return;
+    ui->radioButton->setText("All's Well!");
+    ui->radioButton->toggle();
 
-	for(char i = 0; i < inputSymbols.length(); i++)
-	{
-		list.append(QString::fromLocal8Bit(inputSymbols.toAscii().data() + i, 1));
-		ui->tableWidget->setColumnWidth(i, 520 / ui->tableWidget->columnCount());
-	}
+    QString inputSymbols;
+    i = 0;
+    while(i < text.length())
+    {
+        if(text.at(i) < 97)
+        {
+            i++;
+            continue;
+        }
+        if(i == 0)
+            inputSymbols.append(text.at(i));
+        else
+        {
+            if (!inputSymbols.contains(text.at(i)))
+                inputSymbols.append(text.at(i));
+        }
+        i++;
+    }
 
-	ui->tableWidget->setHorizontalHeaderLabels(list);
+    QStringList list;
 
-	list.clear();
+    ui->tableWidget->setColumnCount(inputSymbols.length());
 
-	for(int i = 0; i <= ui->tableWidget->rowCount(); i++)
-		list.append(tr("q%1").arg(i));
-	ui->tableWidget->setRowCount(ui->tableWidget->rowCount() + 1);
-	ui->tableWidget->setVerticalHeaderLabels(list);
+    for(char i = 0; i < inputSymbols.length(); i++)
+    {
+        list.append(QString::fromLocal8Bit(inputSymbols.toAscii().data() + i, 1));
+        ui->tableWidget->setColumnWidth(i, 520 / ui->tableWidget->columnCount());
+    }
+
+    ui->tableWidget->setHorizontalHeaderLabels(list);
+
+    list.clear();
+
+    for(int i = 0; i <= ui->tableWidget->rowCount(); i++)
+        list.append(tr("q%1").arg(i));
+    ui->tableWidget->setRowCount(ui->tableWidget->rowCount() + 1);
+    ui->tableWidget->setVerticalHeaderLabels(list);
+
 
 }
